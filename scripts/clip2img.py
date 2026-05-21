43465e2bc0879ef90762a8c4138be3b2dc039f9f
scp3500 <3379536483@qq.com>
2026-05-21 08:23:19 +0800

    remove redundant files

--- scripts/clip2img.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-"""从剪贴板读图并调用 OpenVL 描述"""
-import os, sys, subprocess
-script = os.path.join(os.path.dirname(__file__), "vision.py")
-subprocess.run([sys.executable, script, "--clip"] + sys.argv[1:])
